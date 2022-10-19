24331c1d982b34d7a58c13175ecf47e811e1ab54
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:35:56 +0400

    delete

--- README.md
+++
@@ -1,4 +0,0 @@
-# Ibook Сервер 3.0
-`Hello`
-# Тестированаие
-[IP address Ibook server](http://134.0.115.2:9000/)
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
-import uvicorn as uvicorn
-from ibook import app
-
-if __name__ == "__main__":
-   uvicorn.run("ibook:app", host="localhost", port=8010, reload=True)
