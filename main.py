3f45db6b01e655254e6cd2138ac200ea32f51b78
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:44:47 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
