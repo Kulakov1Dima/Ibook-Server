b9685348eacf58c45d45d302374b29e84f8d1756
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:02:09 +0400

    Update ibook.py

--- ibook.py
+++ ibook.py
@@ -1,9 +1,22 @@
-from flask import Flask
-application = Flask(__name__)
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from flask import Flask, escape, request
 
-@application.route("/")
-def hello():
-   return "<h1>ibook server ready 19.10.22</h1>"
+flask_app = Flask(__name__)
 
-if __name__ == "__main__":
-   application.run(host='0.0.0.0')
+
+@flask_app.route("/")
+def flask_main():
+    name = request.args.get("name", "World")
+    return f"Hello, {escape(name)} from Flask!"
+
+
+app = FastAPI()
+
+
+@app.get("/v2")
+def read_main():
+    return {"message": "Hello World"}
+
+
+app.mount("/v1", WSGIMiddleware(flask_app))
