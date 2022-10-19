e2d54590d74bb15d2c44971b581e7a4c5a113cd2
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:29:59 +0400

    Update ibook.py

--- ibook.py
+++ ibook.py
@@ -7,4 +7,4 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="134.0.115.2", port=8000, reload=True)
+    uvicorn.run("ibook:app", port=8000, reload=True)
