024fda82dd4adfcfb3dd1a8883742c8da12582ae
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:07:52 +0400

    Update ibook.py

--- ibook.py
+++ ibook.py
@@ -1,22 +1,11 @@
+import uvicorn as uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
-from flask import Flask, escape, request
-
-flask_app = Flask(__name__)
-
-
-@flask_app.route("/")
-def flask_main():
-    name = request.args.get("name", "World")
-    return f"Hello, {escape(name)} from Flask!"
-
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/v2")
-def read_main():
-    return {"message": "Hello World"}
-
-
-app.mount("/v1", WSGIMiddleware(flask_app))
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8010, reload=True)
