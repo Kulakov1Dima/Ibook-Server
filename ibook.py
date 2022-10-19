5a392fe6942bb36bbbe56d4d576c9f108c16aaa7
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 02:15:44 +0400

    Update ibook.py

--- ibook.py
+++ ibook.py
@@ -2,10 +2,9 @@ import uvicorn as uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8010, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
