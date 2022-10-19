0c171716ef68a44b33fc0376f3b3903647ede7fe
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-20 00:20:12 +0400

    fix problem uvicorn

--- ibook.py
+++ ibook.py
@@ -7,4 +7,4 @@ def root():
    return {"message": "Ibook Server are working for you ;)"}
 
 if __name__ == "__main__":
-   uvicorn.run("ibook:application", port=9000, reload=True)
+   uvicorn.run("ibook:application", host="0.0.0.0", port=9000, reload=True)
