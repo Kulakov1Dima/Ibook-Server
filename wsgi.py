a065e8744ffbca2811a9943a5a1ba9f3a79baa20
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-19 11:54:21 +0400

    add server core "ibook.py"

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-def application(env, start_response):
-	start_response('200 OK', [('Content-Type','text/html')])
-	return [b"<h1>ibook server ready</h1>"]
+from ibook import application
+
+if __name__ == "__main__":
+   application.run()
