2e26b9e8181f4cb780a05a1fed03fef94bbbf542
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-22 23:46:12 +0400

    отладка

--- ibook.py
+++ ibook.py
@@ -10,7 +10,7 @@ def hello():
 @application.route("/auth", methods=["POST"])
 @application.route("/authorization",methods=["POST"])
 def auth():
-   return authorization(request.get_data)
+   return authorization(request.get_data())
 
 if __name__ == "__main__":
    application.run()
