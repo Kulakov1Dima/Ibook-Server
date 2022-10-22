a60f918aca113490ef84751e9b78244dda0bb36e
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-22 23:21:25 +0400

    add authorization server

--- ibook.py
+++ ibook.py
@@ -1,10 +1,16 @@
 import codecs
+from authorizationServer.auth import auth as authorization 
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
    return codecs.open("Hello.html", 'r', "utf-8").read()
+
+@application.route("/auth", methods=["POST"])
+@application.route("/authorization",methods=["POST"])
+def auth():
+   return authorization()
 
 if __name__ == "__main__":
    application.run()
