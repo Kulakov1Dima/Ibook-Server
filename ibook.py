029b510b38d1c4879ee333bba53328c9ca2c73cc
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-10-22 10:58:13 +0400

    added a greeting

--- ibook.py
+++ ibook.py
@@ -1,9 +1,10 @@
+import codecs
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-   return "<h1>ibook server ready 19.10.22</h1>"
+   return codecs.open("Hello.html", 'r', "utf-8").read()
 
 if __name__ == "__main__":
-   application.run(host='0.0.0.0')
+   application.run()
