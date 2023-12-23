afbab78310d281684dcf6fe372cd2b8d1e2511ca
Кулаков Дмитрий / Kulakov Dmitry <84613812+Kulakov1Dima@users.noreply.github.com>
2023-12-23 23:54:06 +0400

    1

--- ibook.py
+++ ibook.py
@@ -1,5 +1,5 @@
-
-import uvicorn as uvicorn
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, HTMLResponse
 
@@ -8,16 +8,16 @@ app = FastAPI()
 file_path = "ibook.apk"
 data_base = "../users.db"
 
-@app.get("/", response_class = HTMLResponse, tags = ["Home"])
-async def home():                                                              # HTML превьюшка
-   with open('Hello.html') as html_file:
+@app.get("/", response_class=HTMLResponse, tags=["Home"])
+async def home():  # HTML превьюшка
+    html_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Hello.html')
+    with open(html_file_path) as html_file:
         data = html_file.read()
-   return HTMLResponse(content = data, status_code = 200)
+    return HTMLResponse(content=data, status_code=200)
 
-@app.get("/version/",  tags = ["Home"])
+@app.get("/version/", tags=["Home"])
 async def update_app():
-    return "2.6.9"                                                             # Текущая актуальная версия приложения до которого будут обновляться старые
+    return "2.6.9"  # Текущая актуальная версия приложения до которого будут обновляться старые
 
 if __name__ == "__main__":
-   #uvicorn.run("ibook:app", host = "127.0.0.1", port = 80, reload = True)      # Запуск сервера для локальной машины
-    uvicorn.run("ibook:app", host = "192.168.1.146",port = 9000, reload = True)
+   uvicorn.run("ibook:app", host="192.168.1.146", port=9000, reload=True)
